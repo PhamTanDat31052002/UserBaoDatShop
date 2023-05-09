a1adab609d2f5c14be9d2eb509d6c5b4a063ac31
Tan Dat <phamtandat31052002@gmail.com>
2023-05-09 21:42:04 +0700

    THêm css product

--- userbaodatshop/src/Components/Header.js
+++ userbaodatshop/src/Components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../Assets/css/style.css"
-
+import { NavLink } from "react-router-dom";
 export default function Header()
 {
     return(
@@ -9,22 +9,23 @@ export default function Header()
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-3">
-                   <div className="logo"><a href="a"><img src={require('../Assets/images/logo.png')} alt=''/></a></div>
+                   <div className="logo"><NavLink to="/"><img src={require('../Assets/images/logo.png')} alt=''/></NavLink></div>
 				</div>
-				<div class="col-sm-9 ">
-					<nav class="navbar navbar-expand-lg navbar-light bg-light">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+				<div className="col-sm-9 ">
+					<nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                         </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                           <a class="nav-item nav-link itemsa" href="index.html">Home</a>
-                           <a class="nav-item nav-link" href="collection.html">Collection</a>
-                           <a class="nav-item nav-link" href="shoes.html">Shoes</a>
-                           <a class="nav-item nav-link" href="racing boots.html">Racing Boots</a>
-                           <a class="nav-item nav-link" href="contact.html">Contact</a>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                           {/* <a className="nav-item nav-link itemsa" href="index.html">Home</a> */}
+                       
+                           <NavLink to="/" className="nav-item nav-link" >Trang chủ</NavLink>
+                           <NavLink to="/product" className="nav-item nav-link" >Sản phẩm</NavLink>
+                        
                            <a className="nav-item nav-link last" href="s"><img src={require('../Assets/images/search_icon.png')} alt=''/></a>
-                           <a className="nav-item nav-link last" href="contact.html"><img src={require('../Assets/images/shop_icon.png')} alt=''/></a>
+                           {/* <a className="nav-item nav-link last" href="contact.html"><img src={require('../Assets/images/shop_icon.png')} alt=''/></a> */}
+                           <NavLink to="/cart" className="nav-item nav-link last" ><img src={require('../Assets/images/shop_icon.png')} alt=''/></NavLink>
                         </div>
                     </div>
                     </nav>
--- userbaodatshop/src/Components/ProductCRUD.js
+++ userbaodatshop/src/Components/ProductCRUD.js
@@ -1,89 +1,84 @@
 import React from "react";
-// import "../Assets/css/bootstrap.min.css"
+import { variable } from "../Variable"
+import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import "../Assets/css/style.css"
 
-export default function ProductCRUD()
-{
-    return(
-        <>
-            <div class="collection_section layout_padding">
-    	<div class="container">
-    		<h1 class="new_text"><strong>New Arrivals Products</strong></h1>
-    	    <p class="consectetur_text">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-    	</div>
-    </div>
-    <div class="layout_padding gallery_section">
-    	<div class="container">
-    		<div class="row">
-    			<div class="col-sm-4">
-    				<div class="best_shoes">
-    					<p class="best_text">Best Shoes </p>
-    					<div class="shoes_icon"><img src={require("../Assets/images/shoes-img4.png")} alt=''/></div>
-    					<div class="star_text">
-    						<div class="left_part">
-    							<ul>
-    	    					<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    						<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    						<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    						<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    						<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    					</ul>
-    						</div>
-    						<div class="right_part">
-    							<div class="shoes_price">$ <span >60</span></div>
-    						</div>
-    					</div>
-    				</div>
-    			</div>
-				<div class="col-sm-4">
-    				<div class="best_shoes">
-    					<p class="best_text">Best Shoes </p>
-    					<div class="shoes_icon"><img src={require("../Assets/images/shoes-img4.png")} alt=''/></div>
-    					<div class="star_text">
-    						<div class="left_part">
-    							<ul>
-    	    					<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    						<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    						<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    						<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    						<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    					</ul>
-    						</div>
-    						<div class="right_part">
-    							<div class="shoes_price">$ <span >60</span></div>
-    						</div>
-    					</div>
-    				</div>
-    			</div>
-				<div class="col-sm-4">
-    				<div class="best_shoes">
-    					<p class="best_text">Best Shoes </p>
-    					<div class="shoes_icon"><img src={require("../Assets/images/shoes-img4.png")} alt=''/></div>
-    					<div class="star_text">
-    						<div class="left_part">
-    							<ul>
-    	    					<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    						<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    						<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    						<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    						<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt=''/></a></li>
-    	    					</ul>
-    						</div>
-    						<div class="right_part">
-    							<div class="shoes_price">$ <span >60</span></div>
-    						</div>
-    					</div>
-    				</div>
-    			</div>
-				
-    		
-    		</div>
-    		
-    		<div class="buy_now_bt">
-    			<button class="buy_text">Buy Now</button>
-    		</div>
-    	</div>
-    </div>
-    
-        </>
-    )
+export default function ProductCRUD() {
+	var location = useLocation();
+	var id = location.state;
+	var [records, setRecord] = useState([]);
+	useEffect(() => {
+		fetch(variable.API_URL + "Products/GetAllProduct")
+			.then(response => response.json())
+			.then(data => setRecord(data)).catch(err => console.log(err))
+	}, [id])
+
+	const [currentPage, setcurrenPage] = useState(1);
+	const recordsPerPage = 5;
+	const lastIndex = currentPage * recordsPerPage;
+	const firstIndex = lastIndex - recordsPerPage;
+	const a = records.slice(firstIndex, lastIndex);
+	const npage = Math.ceil(records.length / recordsPerPage)
+	const numbers = Array.from({ length: npage }, (_, i) => i + 1);
+
+	return (
+		<>
+			<div className="collection_section layout_padding">
+				<div className="container">
+					<h1 className="new_text"><strong>New Arrivals Products</strong></h1>
+					<p className="consectetur_text">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+				</div>
+			</div>
+			<div className="layout_padding gallery_section">
+				<div className="container">
+					<div className="row">
+						{a.map(dep =>
+							<div className="col-sm-4  ">
+								<div className="best_shoes parent ">
+									<p className="best_text "> {dep.name} </p>
+									<div className="shoes_icon imageMax"><img src={require("../Assets/images/" + dep.image)} alt='a' /></div>
+									<div className="star_text " >
+										<div className="left_part">
+											<ul>
+												<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt='' /></a></li>
+												<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt='' /></a></li>
+												<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt='' /></a></li>
+												<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt='' /></a></li>
+												<li><a href="a"><img src={require("../Assets/images/star-icon.png")} alt='' /></a></li>
+											</ul>
+										</div>
+										<div className="right_part">
+											<div className="shoes_price hidden-child"><span >{dep.price}đ</span></div>
+											
+										</div>
+									</div>
+								</div>
+							</div>
+						)}
+
+					</div>
+
+					<div className="buy_now_bt">
+						<button className="buy_text">Buy Now</button>
+					</div>
+				</div>
+			</div>
+
+		</>
+	)
+	function changePage(id) {
+		setcurrenPage(id)
+	}
+	function nextPage() {
+		if (currentPage !== npage) {
+			setcurrenPage(currentPage + 1)
+		}
+	}
+	function prePage() {
+		if (currentPage !== 1) {
+			setcurrenPage(currentPage - 1)
+		}
+	}
 }
